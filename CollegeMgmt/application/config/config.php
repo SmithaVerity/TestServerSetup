9163f0f8e09a6d1d4a353c0fad41f6bf077b9621
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-24 18:54:10 +0530

    Update config.php

--- CollegeMgmt/application/config/config.php
+++ CollegeMgmt/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://52.64.214.228/CollegeMgmt/';
+$config['base_url'] = 'http://localdb-1531372774.ap-southeast-2.elb.amazonaws.com/CollegeMgmt/';
 
 /*
 |--------------------------------------------------------------------------
