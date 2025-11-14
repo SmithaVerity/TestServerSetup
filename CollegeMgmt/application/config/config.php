5cab8e12f218e8c98ff1799051e5d181338753ca
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-11-14 22:30:09 +0530

    Update base_url to use AWS ELB endpoint

--- CollegeMgmt/application/config/config.php
+++ CollegeMgmt/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://3.105.163.192/CollegeMgmt/';
+$config['base_url'] = 'http://localdb-714317443.ap-southeast-2.elb.amazonaws.com/CollegeMgmt/';
 
 /*
 |--------------------------------------------------------------------------
